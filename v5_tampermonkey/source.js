015d88e5c58ade098e40d915cb635b2452ef0df0
zweix123 <74446314+zweix123@users.noreply.github.com>
2022-03-16 12:23:34 +0800

    Update and rename 油猴js脚本做B站分P视频总进度.js to source.js

--- v5_tampermonkey/source.js
+++ v5_tampermonkey/source.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         B站合集视频总进度栏
+// @name         B站合集总进度栏
 // @namespace    http://tampermonkey.net/
-// @version      0.5
-// @description  B站在有分P的视频中，只有集数的情况，而没有从时间上考虑已看多少，此插件在打开分匹配视频是会在视频名旁边弹出这方面情况，在改变分P时要手动点击弹出的窗口来更新
+// @version      0.1
+// @description  用于B站的合集在时间维度上的总进度，设计时尽可能解耦，利于个性化修改
 // @author       zweix
 // @match        https://www.bilibili.com/video/*
 // @icon         https://www.bilibili.com/favicon.ico
@@ -14,20 +14,21 @@
 
 //javascript:( //function () {
 
-window.onload = function () {  //入口
-    void function (time) {  //匿名的sleep函数，等网页加载完
+window.onload = function () { //入口
+    let number = 2;  //等待网页加载时间，这里选择2秒，万一不行呢
+    void function (time) {  //匿名的sleep函数，用于等网页加载完
         return new Promise((resolve) => setTimeout(resolve, time));
-    } (2 * 1000).then(() => {  //等两秒
-        let list = document.getElementsByClassName('cur-page');  //查看合集的页数，没有分P的长度是0的
+    } (number * 1000).then(() => {  //等两秒
+        let list = document.getElementsByClassName('cur-page');  //获得分P栏的表头，如果不是合集其长度为0
         if (list.length == 0) return ;
         solve();
     })
 }
-function get(num) {  //对于一个索引，从开始到这个索引的时间
+function get(num) {  //对于一个索引，得到从合集开始到这个索引视频的一些时间
     let hour = 0, minute = 0, second = 0;
     for (let i = 0; i < num; ++ i) {
         let vec = document
-            .getElementsByClassName('duration')[i]
+            .getElementsByClassName('duration')[i]  //这个是每一个栏的信息
             .innerHTML.match(/\d+/g);
         if (vec.length == 2) vec.unshift('0');
         hour += parseInt(vec[0]);
@@ -41,11 +42,11 @@ function get(num) {  //对于一个索引，从开始到这个索引的时间
     let str = hour + ':' + minute + ':' + second;
     let sum = hour * 3600 + minute * 60 + second
 
-    return {index, str , sum};  //返回这个数的索引、时间（字符串）、总用时（数字）
+    return {index, str , sum};  //返回这集视频的索引（为了统一）、时间 （字符串）、总用时（数字）这三者的元组（js是叫这个吧--手动捂脸--）
 }
 
 function solve() {
-    //获得所有——当前的和最后的
+    //分P栏的表头，利用正则表达式提取出想要的信息
     let cur = parseInt(document
         .getElementsByClassName('cur-page')[0]
         .innerHTML.match(/(\d+)\//)[1]);
@@ -54,14 +55,14 @@ function solve() {
         .innerHTML.match(/\/(\d+)/)[1]);
     cur = get(cur)
     end = get(end)
-    //展示
+    //显示出来
     show(cur, end)
 }
 
 function show(cur, end) {
     let plain = document.getElementsByClassName('video-data')[0];  //确定位置
     let sam = document.getElementById('button_tag_zweix');  //原因见下
-    let isNULL = (sam === null);  //检测这个按钮有无，如果没有，就建立，如果已经有，说明之前创建过，就在其基础上修改
+    let isNULL = (sam === null);  //因为这个地方要更新，这里的策略是如果没有就创建，没有就在其基础上修改
 
     if (isNULL) {
         sam = document.createElement('button');
@@ -74,9 +75,8 @@ function show(cur, end) {
             '    margin-left: 1rem;\n' +
             '    padding:0.5rem;\n' +
             '    cursor: pointer;\n' +
-            //'    border-radius: 1rem;\n' +  //借鉴的代码是圆角的，确实更好看，但是按钮的阴影会突兀
             '    ';
-        sam.addEventListener("click", solve);  //借鉴的代码是计时然后周期性更新，我采用点击按钮手动更新
+        sam.addEventListener("click", solve);  //更新方案，手动点击按钮来更新，为何不采用更自动的方案看说明
     }
 
     sam.innerHTML = "现在是第" + cur.index + "集，共用时" + cur.str + ";  已完成" + (cur.sum / end.sum * 100).toFixed(2) + "%";
